b3bea51c034c849e5a15ef4d465b43f8bcbc8b9b
Анастасия <ll-anastasiia.medvedeva-ll@yandex.ru>
2019-11-18 00:25:54 +0300

    Updates

---
+++ precache-manifest.87dbda2697a3fc04e847bc58d4178543.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "026afdcdce35fe2afae4",
+    "url": "/Funbox-task/static/js/runtime~main.6226e8ef.js"
+  },
+  {
+    "revision": "141001ed1bbd54e35e19",
+    "url": "/Funbox-task/static/js/main.9a2e3910.chunk.js"
+  },
+  {
+    "revision": "e743bd6f9f3ff14d1901",
+    "url": "/Funbox-task/static/js/2.f5614e40.chunk.js"
+  },
+  {
+    "revision": "141001ed1bbd54e35e19",
+    "url": "/Funbox-task/static/css/main.e466c52f.chunk.css"
+  },
+  {
+    "revision": "b348f65eb4e67487510bb5a1138d4f02",
+    "url": "/Funbox-task/index.html"
+  }
+];
